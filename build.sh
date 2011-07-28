d44e7f7b1fc53b248e0dcff27f8028e931c20dac
Davis Clark <davis.clark@net-machine.com>
2011-07-28 16:53:46 -0500

    Modified build script. jquery.formwarden.min.js is the minified combination of formwarden.js and jquery.formwarden.js.

--- build.sh
+++ build.sh
@@ -1,1 +1,8 @@
-java -jar ./lib/compiler.jar --compilation_level SIMPLE_OPTIMIZATIONS --js ./jquery.formwarden.js --js_output_file ./jquery.formwarden.min.js
+# Combined minification
+cat ./formwarden.js > build.tmp
+cat ./jquery.formwarden.js >> build.tmp
+java -jar ./lib/compiler.jar --compilation_level SIMPLE_OPTIMIZATIONS --js ./build.tmp --js_output_file ./jquery.formwarden.min.js
+rm build.tmp
+
+# formwarden.js
+java -jar ./lib/compiler.jar --compilation_level SIMPLE_OPTIMIZATIONS --js ./formwarden.js --js_output_file ./formwarden.min.js
