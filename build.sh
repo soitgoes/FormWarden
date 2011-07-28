a5dfa7363eabfa2ec73a17ed414d28db9c00590e
Davis Clark <davis.clark@net-machine.com>
2011-07-28 17:25:46 -0500

    removed unnecessary step from the build script

--- build.sh
+++ build.sh
@@ -1,13 +1,10 @@
-# Combined minification
-cat ./formwarden.js > ./build.tmp
-echo ";" >> ./build.tmp #just to be safe
-cat ./jquery.formwarden.js >> ./build.tmp
-echo ";" >> build.tmp # just to be safe
-java -jar ./lib/compiler.jar --compilation_level SIMPLE_OPTIMIZATIONS --js ./build.tmp --js_output_file ./jquery.formwarden.min.js
-rm ./build.tmp
-
 # formwarden.js
 cat ./formwarden.js > ./build.tmp
 echo ";" >> ./build.tmp
 java -jar ./lib/compiler.jar --compilation_level SIMPLE_OPTIMIZATIONS --js ./build.tmp --js_output_file ./formwarden.min.js
-rm build.tmp
+
+# Combined minification
+cat ./jquery.formwarden.js >> ./build.tmp
+echo ";" >> build.tmp # just to be safe
+java -jar ./lib/compiler.jar --compilation_level SIMPLE_OPTIMIZATIONS --js ./build.tmp --js_output_file ./jquery.formwarden.min.js
+rm ./build.tmp
