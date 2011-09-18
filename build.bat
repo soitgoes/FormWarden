50568a5edf46ad93b95bd7c2533cb2738dbaf48f
Martin Murphy <martin.murphy@whiteboard-it.com>
2011-09-17 22:24:21 -0500

    alter build to output to build folder

--- build.bat
+++ build.bat
@@ -1,10 +1,10 @@
 copy NUL build.tmp
 more formwarden.js > build.tmp
 echo ; >> build.tmp
-java -jar ./lib/compiler.jar --compilation_level SIMPLE_OPTIMIZATIONS --js ./build.tmp --js_output_file ./formwarden.min.js
+java -jar ./lib/compiler.jar --compilation_level SIMPLE_OPTIMIZATIONS --js ./build.tmp --js_output_file ./build/formwarden.min.js
 
 
 more jquery.formwarden.js >> build.tmp
 echo ; >> build.tmp
-java -jar ./lib/compiler.jar --compilation_level SIMPLE_OPTIMIZATIONS --js ./build.tmp --js_output_file ./jquery.formwarden.min.js
+java -jar ./lib/compiler.jar --compilation_level SIMPLE_OPTIMIZATIONS --js ./build.tmp --js_output_file ./build/jquery.formwarden.min.js
 del build.tmp
