9d2161895c46997753a723838236d481c9f58025
Lewis Patterson <lewis.patterson@whiteboard-it.com>
2014-04-03 12:50:25 -0500

    Changes to support user-defined functions.

--- formwarden.js
+++ formwarden.js
@@ -1,4 +1,4 @@
-(function(definition) {
+(function (definition) {
 
   // This file will function properly as a <script> tag, or a module
   // using CommonJS and NodeJS or RequireJS module formats.  In
@@ -16,7 +16,7 @@
     definition(void 0, fw = {});
   }
 
-})(function(serverSideRequire, exports) {
+})(function (serverSideRequire, exports) {
   "use strict";
 
   var path = !serverSideRequire ? null : serverSideRequire('path'),
@@ -31,7 +31,7 @@
   // Establish the object that gets returned to break out of a loop iteration.
   var breaker = {};
 
-  var each = function(obj, iterator, context) {
+  var each = function (obj, iterator, context) {
     if (obj == null) return;
     if (nativeForEach && obj.forEach === nativeForEach) {
       obj.forEach(iterator, context);
@@ -49,8 +49,8 @@
   };
 
   // Extend a given object with all the properties in passed-in object(s).
-  var extend = function(obj) {
-    each(slice.call(arguments, 1), function(source) {
+  var extend = function (obj) {
+    each(slice.call(arguments, 1), function (source) {
       for (var prop in source) {
         if (source[prop] !== void 0) obj[prop] = source[prop];
       }
@@ -60,43 +60,43 @@
   /* End Taken from Underscore.js */
 
   var defaultValidators = {
-    required: function(value) {
+    required: function (value) {
       if (value === undefined || value === null) {
         return true;
       }
       return value.length || value !== "";
     },
-    email: function(value) {
+    email: function (value) {
       if (value) {
         return value.match(/\b[A-Z0-9._%-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b/i);
       }
       return true;
     },
-    ssn: function(value) {
+    ssn: function (value) {
       if (value) {
         return value.match(/\b[0-9]{3}-[0-9]{2}-[0-9]{4}\b/);
       }
       return true;
     },
-    date: function(value) {
+    date: function (value) {
       if (value) {
         return value.match(/(0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])[- /.](19|20)[0-9]{2}/);
       }
       return true;
     },
-    iso8601: function(value) {
+    iso8601: function (value) {
       if (value) {
         return value.match(/^(\d{4})\D?(0[1-9]|1[0-2])\D?([12]\d|0[1-9]|3[01])$/);
       }
       return true;
     },
-    maxLength: function(value, form, validation) {
+    maxLength: function (value, form, validation) {
       if (value) {
         return value.length <= validation.length;
       }
       return true;
     },
-    minLength: function(value, form, validation) {
+    minLength: function (value, form, validation) {
       if (value) {
         return value.length >= validation.length;
       }
@@ -104,59 +104,61 @@
     }
   };
 
-  var isInvalidField = function(form, key, fieldOptions, validators, conventionBased) {
+  var isInvalidField = function (form, key, fieldOptions, validators, conventionBased) {
     if (!fieldOptions) {
       return true;
     }
     var value = form[key];
     var checkVisibility = fieldOptions.push ? false : true;
-    //if there is not a visibility function or it is true then execute the validation
+    // If there is not a visibility function or it is true then execute the validation.
     var validations = checkVisibility ? fieldOptions.validations : fieldOptions;
 
     for (var i = 0; i < validations.length; i++) {
       var validation = validations[i];
-      if (validation.isValid.test) { //regex
-        var pattern = validation.isValid;
-        if (!pattern.test(value)) {
-          return validation.message || key + " is invalid";
-        }
-      } else if (typeof validation.isValid == "function") {
+      if (typeof validation.isValid == "function") {
         if (!validation.isValid(value, form)) {
           return validation.message || key + " is invalid";
         }
-      } else {
-        var validFx = validators[validation.isValid];
-        if (validFx) {
-          if (!validFx(value, form, validation)) {
-            if (validation.invalid) {
-              validation.invalid();
-            }
+      }else{
+        if (validation.isValid.test) { // regex
+          var pattern = validation.isValid;
+          if (!pattern.test(value)) {
             return validation.message || key + " is invalid";
-          } else {
-            if (validation.valid) {
-              validation.valid();
-            }
           }
         } else {
-          console.log("Validation Function not found: " + validation.isValid)
+          var validFx = validators[validation.isValid];
+          if (validFx) {
+            if (!validFx(value, form, validation)) {
+              if (validation.invalid) {
+                validation.invalid();
+              }
+              return validation.message || key + " is invalid";
+            } else {
+              if (validation.valid) {
+                validation.valid();
+              }
+            }
+          } else {
+            console.log("Validation Function not found: " + validation.isValid)
+          }
         }
       }
     }
     return "";
   }
-  var isVisibleField = function(form, fieldOptions) {
+  var isVisibleField = function (form, fieldOptions) {
     if (fieldOptions && fieldOptions.isVisible && typeof fieldOptions.isVisible == "function" && !fieldOptions.isVisible(form)) {
       return false;
     }
     return true;
   }
-  //if server side don't bother suppling fieldsEntered
-  exports.validateForm = function(form, validationOptions) {
+  // If server side don't bother supplying fieldsEntered.
+  exports.validateForm = function (form, validationOptions) {
     var fields = {};
     var validForm = true;
     var validators = extend(defaultValidators, validationOptions.validators || {});
 
-    each(validationOptions.fields, function(val, key) {
+    each(validationOptions.fields, function (val, key) {
       var visible = isVisibleField(form, val),
         mesg = isInvalidField(form, key, val, validators, validationOptions.conventionBased),
         valid = (!visible) || (visible && mesg === ""),
@@ -167,7 +169,7 @@
       result = {
         visible: visible,
         error: mesg,
-        valid: !! valid
+        valid: !!valid
       };
 
       if (result) {
@@ -175,14 +177,14 @@
       }
     });
 
-    each(form, function(val, key) {
+    each(form, function (val, key) {
       // Form fields that have no validators are visible and valid
       if (validationOptions.conventionBased) {
-        //loop through all of the validators if any of them partial match then execute them and return
+        // Loop through all of the validators if any of them partial match then execute them and return.
         if (val != "") {
           for (var vkey in validators) {
             if (key.indexOf(vkey) > -1) {
-              var valid = !! validators[vkey](val, form);
+              var valid = !!validators[vkey](val, form);
               var mesg = valid ? "" : validators[vkey].message || key + " is invalid";
               fields[key] = {
                 visible: true,
@@ -205,11 +207,11 @@
 
     return {
       fields: fields,
-      validForm: !! validForm
+      validForm: !!validForm
     };
   }
 
-  exports.getFormWardenScriptContents = function() {
+  exports.getFormWardenScriptContents = function () {
     if (!path || !fs) {
       console.log('This is not supported as a client side function.')
       return;
@@ -218,7 +220,7 @@
     return fs.readFileSync(formWardenFilePath, 'utf8')
   }
 
-  exports.getJqueryFormWardenScriptContents = function() {
+  exports.getJqueryFormWardenScriptContents = function () {
     if (!path || !fs) {
       console.log('This is not supported as a client side function.')
       return;
